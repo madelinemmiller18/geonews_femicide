2867e987c0c586bb10f1c1fe79d47dd02f53bad0
Madeline Miller <madeline.m.miller.18@gmail.com>
2026-01-28 10:31:35 +0100

    update - madeline explore and topk files

--- source/create_csv_thresholds.py
+++ source/create_csv_thresholds.py
@@ -10,7 +10,7 @@ print(f'df 32 raw shape: {df_32_raw.shape}')
 
 df_32 = df_32_raw
 
-"""
+
 #Filter to combine NUTS into a single comma separated value -----------------------------
 #make all NUTS a string
 df_32_raw['NUTS'] = df_32_raw['NUTS'].fillna('').astype(str)
@@ -28,7 +28,7 @@ df_32 = df_32[df_32['NUTS'].notna()]
 df_32 = df_32[df_32['NUTS']!='']
 
 print(f'df 32 nuts condensed shape: {df_32.shape}')
-"""
+
 
 #Filter to 2017 - 2032 -----------------------------
 #Convert the date column to datetime objects
@@ -40,5 +40,20 @@ print(f'df 32 date removed shape: {df_32.shape}')
 #Filter to cosine distance >= .225 -----------------------------
 df_32 = df_32[df_32['cos_dist']<=.225].copy()
 print(f'shape of df_32_threshold: {df_32.shape}')
+
+"""
+#group by all cols of interest to make sure no other dup causes
+df_32_grouped = df_32.groupby(['id', 'NUTS']).agg({
+    'url': 'first',
+    'hostname': 'first',
+    'loc_normal': "first",
+    'latitude': 'first',
+    'longitude': 'first'
+    }).reset_index()
+
+print(f'shape of df_32 grouped: {df_32_grouped.shape}')
+"""
+
+print(f'shape of df_32 final: {df_32.shape}')
 
 df_32.to_csv(f'{csv_output_path}q32_filterdate-threshold.csv', index=False)
